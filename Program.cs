c3f2d302558a63608b562eb919214861495cb71b
SignorOmbra <SignorOmbra@gmail.com>
2020-08-20 10:06:16 -0400

    Fixed a major bug where commas were not added to the end of an array.

--- Program.cs
+++ Program.cs
@@ -90,7 +90,7 @@ namespace IMG2RBX {
                                 );
                             }
 
-                            AddText(luaFile, "}");
+                            AddText(luaFile, "},");
                         }
 
                         AddText(luaFile, "}");
